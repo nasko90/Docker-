5c962ca49f838bb7dca02ef25cedbcdf1a4922f5
nasko90 <nasko_drenovichki@abv.bg>
2019-01-12 11:42:26 +0200

    first commit

---
+++ docker/shared/Vafla.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace shared
+{
+    public class Vafla  
+    {
+        public Vafla()
+        {
+            Borovets = "Super e";
+        }
+        public string Borovets { get; set; }
+    }
+}
